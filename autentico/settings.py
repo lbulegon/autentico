c6603450d7d71826fb402fce2ac9c92a68f9bff7
lbulegon <lbulegon@gmail.com>
2024-12-07 22:56:30 -0300

    banco

--- autentico/settings.py
+++ autentico/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api_001',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,25 @@ WSGI_APPLICATION = 'autentico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'WSxmVOLlEaSnAWddVAkqxEDHcgaZQwrK',
+        'HOST':'mysql.railway.internal',
+        'PORT':'3306',
     }
 }
+
+
 
 
 # Password validation
